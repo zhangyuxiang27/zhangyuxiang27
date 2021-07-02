e099defea92c378a7c3a6eff6103d613dba87ebe
zhangyuxiang27 <zhangyuxiang27@163.com>
2021-07-02 16:06:27 +0800

    Site updated: 2021-07-02 16:06:26

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -73,6 +73,8 @@ $(document).ready(function () {
   $('.post-toc a').on('click', function (e) {
     e.preventDefault();
     var targetSelector = NexT.utils.escapeSelector(this.getAttribute('href'));
+    // <!-- 添加下面这行代码, 重新解析 URL ->
+    targetSelector = decodeURI(this.getAttribute('href'))
     var offset = $(targetSelector).offset().top;
 
     hasVelocity ?
